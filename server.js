094e6d23c1fedc7586b3a521cbae088cb176672a
Yaya Mamoudou <yayamamoudou0@gmail.com>
2026-04-30 19:30:36 +0100

    chore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-res.send(`Hello World on ${Date.UTC().toISOString()}`);
+res.send(`Hello World on ${Date.UTC().toLocaleString()}`);
 });
 
 app.get("/random-numbers", (req, res) => {
